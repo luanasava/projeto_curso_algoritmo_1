1c2e7be98cfe4d6da91296d68235581ab0894df4
Luana Sava <luanasava@gmail.com>
2020-07-16 23:13:33 -0300

    Primeiro commite do código

---
+++ estrutura-escolha.js
@@ -0,0 +1,22 @@
+var valor1, valor2, resultado, operacao
+
+function acaoBotao(params) {
+   valor1 = prompt("Digite o primeiro valor: ")
+   operacao = prompt("Digite a operação + - * /: ")
+   valor2 = prompt("Digite o segundo valor: ")
+   switch (operacao) {
+       case "+":
+           resultado = parseInt( valor1 ) + ( valor2 )
+           break;   
+        case "-":
+            resultado = parseInt( valor1 ) - ( valor2 )
+            break;
+        case "*":
+            resultado = parseInt( valor1 ) * ( valor2 )
+            break;
+        case "/":
+            resultado = parseInt( valor1 ) / ( valor2 )
+            break;           
+   } 
+   document.getElementById("paragrafo").innerText = resultado         
+}
